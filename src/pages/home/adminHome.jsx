33a51fb14036411dba95079363a8d6d8c4ae433e
Gian Marco <mercanzinigianmarco@gmail.com>
2025-11-11 20:05:06 -0300

    se arregla bug visual en adminHome

--- src/pages/home/adminHome.jsx
+++ src/pages/home/adminHome.jsx
@@ -4,7 +4,7 @@ import '../../styles/main.css';
 export default function AdminHome() {
   return (
     <div className="contenedor-body">
-      <h2>Panel de Administración 🛠️</h2>
+      <h2>Panel de Administración</h2>
       <h5>
         Gestioná torneos, juegos, inscripciones y usuarios desde el panel superior.
       </h5>
